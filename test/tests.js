68e918f7a2778f3edab27f933ab7005cfdabd037
Lilly Richardson <lilly.richardson@gmail.com>
2021-07-15 16:38:34 -0700

    adding tests to see if they pass

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,105 @@
 // IMPORT MODULES under test here:
 // import { add } from '../calculator.js';
+import { add, subtract, multiply, divide } from '../utils.js';
 
+// using a test library
 const test = QUnit.test;
 
-// name your test by what it is testing
-test('time to test a function', (expect) => {
+test('add should take in 5 and 3 and return 8', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 8;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(5, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('add should take in 23 and -10 and return 13', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 13;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(23, -10);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('add should take in 100 and 0 return 100', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 100;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(100, 0);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('add should take in "5" and 3 return 8', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 8;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add('5', 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('subtract should take in 10 and 5 and return 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(10, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('multiply should take in 5 and 5 and return 25', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 25;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(5, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('divide should take in 12 and 3 and return 4', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 4;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divide(12,3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
---
+++ utils.js
@@ -0,0 +1,27 @@
+export function add(firstNumber, secondNumber) {
+
+return Number(firstNumber) + Number(secondNumber);
+
+}
+
+
+export function subtract(firstNumber, secondNumber) {
+
+    return Number(firstNumber) - Number(secondNumber);
+    
+    }
+
+    export function multiply(firstNumber, secondNumber) {
+
+        return Number(firstNumber) * Number(secondNumber);
+        
+        }
+
+        export function divide(firstNumber, secondNumber) {
+
+            return Number(firstNumber) / Number(secondNumber);
+            
+            }
+
+
+
